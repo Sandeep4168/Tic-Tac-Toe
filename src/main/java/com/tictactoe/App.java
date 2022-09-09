6ccde9c8c311d954fe213030e17549e4148ae94b
Sandeep Yadawad <sandu4168@gmail.com>
2022-09-09 21:21:06 +0530

    Initial commit

---
+++ src/main/java/com/tictactoe/App.java
@@ -0,0 +1,18 @@
+package com.tictactoe;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        
+        TicTacToe tictactoe = new TicTacToe();
+    }
+}
